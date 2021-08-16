5522778d9196d490eebf6e3aee70be57c88c8abc
Sabi Viji <sabipalathinkal10@gmail.com>
2021-08-16 23:47:51 +0530

    Input in java

---
+++ Java+DSA/first tutorial/Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        System.out.print("Enter a number : ");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
+    }
+}
