77fc3cc945a96e6cf3e80c57275aad2510bdd021
Sabi Viji <sabipalathinkal10@gmail.com>
2021-08-17 00:04:21 +0530

    Primitive Datatypes

---
+++ Java+DSA/first tutorial/Primitive.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Primitive {
+    public static void main(String[] args) {
+        System.out.println("Working");
+        //Primitive Data types
+        int rollno = 54;
+        char name = 'g';
+        float marks = 874.67f;
+        double large = 8768712684789898.2133;
+        long longint = 6685465L;
+        boolean check = true;
+    }
+}
