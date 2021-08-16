1027622305046c7b65883b62c9bd0dd1278fd0f5
Sabi Viji <sabipalathinkal10@gmail.com>
2021-08-17 00:42:53 +0530

    Loops

---
+++ Java+DSA/first tutorial/Loop.java
@@ -0,0 +1,16 @@
+public class Loop {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        
+        //While loop
+        int count = 1;
+        while(count!=5){
+            System.out.println(count);
+            count++;
+        }
+
+        for(int count2=1;count2!=5;count2++){
+            System.out.println(count2);
+        }
+    }
+}
